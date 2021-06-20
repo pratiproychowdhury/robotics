3b529b8380c84bc321fd2e1a0efd6c72e753f1fb
pratiproychowdhury <30447948+pratiproychowdhury@users.noreply.github.com>
2021-06-20 23:03:42 +0200

    Update robot_sim_monte_carlo.cpp

--- robot_sim_monte_carlo.cpp
+++ robot_sim_monte_carlo.cpp
@@ -224,26 +224,78 @@ void visualization(int n, Robot robot, int step, Robot p[], Robot pr[])
 }
 */
 
-//####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 int main()
 {
-    // Instantiate a robot object from the Robot class
+    //Practice Interfacing with Robot Class
     Robot myrobot;
-
-    // Set robot new position to x=30.0, y=50.0 and orientation=PI/2.0
+    myrobot.set_noise(5.0, 0.1, 5.0);
     myrobot.set(30.0, 50.0, M_PI / 2.0);
-
-    // Turn clockwise by PI/2.0 and move by 15.0 meters
     myrobot.move(-M_PI / 2.0, 15.0);
-
-    // Print the distance from the robot toward the eight landmarks
-    cout << myrobot.read_sensors() << endl;
-
-    // Turn clockwise by PI/2.0 and move by 10.0 meters
+    //cout << myrobot.read_sensors() << endl;
     myrobot.move(-M_PI / 2.0, 10.0);
+    //cout << myrobot.read_sensors() << endl;
 
-    // Print the distance from the robot toward the eight landmarks
-    cout << myrobot.read_sensors() << endl;
+    // Create a set of particles
+    int n = 1000;
+    Robot p[n];
 
+    for (int i = 0; i < n; i++) {
+        p[i].set_noise(0.05, 0.05, 5.0);
+        //cout << p[i].show_pose() << endl;
+    }
+
+    //Re-initialize myrobot object and Initialize a measurment vector
+    myrobot = Robot();
+     vector<double> z;
+
+    //Iterating 50 times over the set of particles
+    int steps = 50;
+    for (int t = 0; t < steps; t++) {
+
+        //Move the robot and sense the environment afterwards
+        myrobot = myrobot.move(0.1, 5.0);
+        z = myrobot.sense();
+
+        // Simulate a robot motion for each of these particles
+        Robot p2[n];
+        for (int i = 0; i < n; i++) {
+            p2[i] = p[i].move(0.1, 5.0);
+            p[i] = p2[i];
+        }
+
+        //Generate particle weights depending on robot's measurement
+        double w[n];
+        for (int i = 0; i < n; i++) {
+            w[i] = p[i].measurement_prob(z);
+            //cout << w[i] << endl;
+        }
+
+        //Resample the particles with a sample probability proportional to the importance weight
+        Robot p3[n];
+        int index = gen_real_random() * n;
+        //cout << index << endl;
+        double beta = 0.0;
+        double mw = max(w, n);
+        //cout << mw;
+        for (int i = 0; i < n; i++) {
+            beta += gen_real_random() * 2.0 * mw;
+            while (beta > w[index]) {
+                beta -= w[index];
+                index = mod((index + 1), n);
+            }
+            p3[i] = p[index];
+        }
+        for (int k=0; k < n; k++) {
+            p[k] = p3[k];
+            //cout << p[k].show_pose() << endl;
+        }
+
+        //####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
+        
+        //Evaluate the error by priting it in this form:
+        // cout << "Step = " << t << ", Evaluation = " << ErrorValue << endl;
+        cout << "Step = " << t << ", Evaluation = " << evaluation(myrobot, p, n) << endl;
+
+    } //End of Steps loop
     return 0;
 }
